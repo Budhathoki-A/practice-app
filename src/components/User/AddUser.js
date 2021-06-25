fdff7ce22af72016b07e83269dc4431e393156be
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-06-25 14:22:17 +1000

    Form build up from scratch with its sytylling

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState('');
   const onSubmitHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <label htmlFor='username'>Username</label>
-      <input type='text' placeholder='Enter your username here!' />
-      <label htmlFor='age'>Age (In Years)</label>
-      <input type='text' placeholder='Enter your age (in years) here' />
-      <button type='submit'>Register</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' />
+        <label htmlFor='age'>Age (In Years)</label>
+        <input type='text' />
+        <Button type='submit'>Add User</Button>
+      </form>
+    </Card>
   );
 };
 
