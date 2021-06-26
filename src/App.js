6fde688e1612018132a6ef353194f1d47390f78e
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-06-26 12:57:53 +1000

    list is added on page with form submission called

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import AddUser from './components/User/AddUser';
 import UsersList from './components/User/UsersList';
 
 function App() {
+  // This is for lifting the state up:
+  const [usersList, setUsersList] = useState([]);
+
+  // This function comes from AddUser as props and we here will handle it as
+  // event but the real event is goin on in AddUser submit button
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
