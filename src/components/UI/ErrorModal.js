d426918d871e5285820e45f4144bcf521090418a
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-07-13 21:41:32 +1000

    final commit for practice

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -3,28 +3,23 @@ import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onChange} />;
-};
-
-const ModalOverlay = (props) => {
-  return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-          <footer className={classes.actions}>
-            <Button onClick={props.onChange}>Okay</Button>
-          </footer>
-        </div>
-      </header>
-    </Card>
-  );
-};
-
 const ErrorModal = (props) => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <div>
+      <div className={classes.backdrop} onClick={props.onChange} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+          <div className={classes.content}>
+            <p>{props.message}</p>
+            <footer className={classes.actions}>
+              <Button onClick={props.onChange}>Okay</Button>
+            </footer>
+          </div>
+        </header>
+      </Card>
+    </div>
+  );
 };
 
 export default ErrorModal;
